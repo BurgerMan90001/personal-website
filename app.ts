ede431704923b9a28d69b5ab61687ce3a61451c1
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-08-15 18:44:55 -0500

    gfhdfghdfhg

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
 import express from 'express'; // Import the express in typescript file
-import dotenv from 'dotenv';
 import { tasksController } from './src/controllers/tasksContoller'; // Import the task routes
 import path from 'path'; 
-const showRequests: boolean = true;
 
 const app: express.Application = express(); // Initialize the express engine
 
-dotenv.config(); // load the env file
-
-const port: string = process.env.PORT || "3000"; // Take a port 3000 for running server.
-const site: string = `http://localhost:${port}/`;
 const publicPath: string = path.join( __dirname + '/public'); // Define the public path
 
 app.use(express.json()); // Add this line to enable JSON parsing in the request body
@@ -20,21 +14,11 @@ app.use('/tasks', tasksController); // Add this line to mount the Task API route
 //console.log(publicPath);
 app.use(express.static(publicPath));
 
-// show requests in console
-if (showRequests) {
-    app.use('/', (request, response, next) => {
-        console.log('Request Type:', request.method, request.url);
-        next();
-    })
-};
 
-// tasks 
+// tasks testing
 app.get('/tasks', function (req, res) {
     console.log("/tasks request called");
     res.send('TASKS API is working');
 });
 
-// starts a simple http server locally on port 3000
-app.listen(port, () => {
-    console.log("listening on : " + site); // show site
-}); // start the server on the port
+export { app }; 
---
+++ server.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import { app } from './app';
+
+dotenv.config(); // load the env file
+
+const showRequests: boolean = true;
+const port: string = process.env.PORT || "3000"; // Take a port 3000 for running server.
+const siteUrl: string = `http://localhost:${port}/`;
+
+// show requests in console
+if (showRequests) {
+    app.use('/', (request, response, next) => {
+        console.log('Request Type:', request.method, request.url);
+        next();
+    })
+};
+
+
+// starts a simple http server locally on port 3000
+app.listen(port, () => {
+    console.log("listening on : " + siteUrl); // show site
+}); // start the server on the port
