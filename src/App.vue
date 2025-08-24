86aeab0190c682434f64ad47f67ff48dbb9080bb
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-08-23 22:35:59 -0500

    dinosaur image

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,4 @@
 
 <template>
-
   <RouterView></RouterView>
-  <!--
-
-  target="_blank" rel="noopener noreferrer" for new tab hyperlinks just in case
-  </head>
-  -->
-  <!--
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="128" height="128" />
-    -->
-
 </template>
--- src/web/layouts/HomeLayout.vue
+++ src/web/layouts/HomeLayout.vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
-import MainTitleContainer from '../components/MainTitleContainer.vue';
+import MainTitleText from '../components/MainTitleText.vue'
 </script>
 
 <template>
-  <div class ='flex-layout'>
-    <MainTitleContainer class="main-title-container" title="This is my website"/>
-
+  <div class="flex-layout">
+    <header class="main-header"></header>
+    <main>
+      <section class="main-title-container">
+        <MainTitleText class="main-title-text" title="This is my website" />
+      </section>
+      <img src="../assets/website-dinosaur.png" class="dinosaur-image" />
+    </main>
+    <footer></footer>
   </div>
-
 </template>
 
 <style scoped>
-.main-title-container {
-  grid-area: header;
+.flex-layout {
+  height: 100%;
+  /*display: flex;*/
+  place-items: center;
+
+  /*background-color: aqua;*/
 }
 
+main {
+  height: 100%;
+}
+.main-title-container {
+  margin: 10rem;
+  background-color: aqua;
+  /*height: 100%*/
+}
+.dinosaur-image {
 
+}
+.main-header {
+  height: 20%;
+  width: 100%;
+  background-color: aqua;
+}
 </style>
