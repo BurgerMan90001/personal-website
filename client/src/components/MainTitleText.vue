76434547482aa38ab5bc3a133ba3e3f21812c8d4
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-08-26 11:18:18 -0500

    fixed main title text alignment

--- client/src/components/MainTitleText.vue
+++ client/src/components/MainTitleText.vue
@@ -9,27 +9,32 @@ defineProps<{
 <template>
 
   <div class="title-text">
-    <h1 class="green">{{ title }}</h1>
-    <h3>{{subHeader}}</h3>
+    <h1 class="sage-soft-text">{{ title }}</h1>
+    <h4 class="sage-text">{{subHeader}}</h4>
     <RouterLinks></RouterLinks>
   </div>
 
 </template>
 
 <style scoped>
+
+template {
+
+}
+h4 {
+  padding: 3px;
+  font-size: 1rem;
+}
 h1 {
   font-weight: 500;
   font-size: 2rem;
-  /*position: relative;*/
-
 }
 
 h3 {
   font-size: 1.2rem;
 }
 
-.title-text h1,
-.title-text h3 {
-  text-align: center;
+.title-text {
+  text-align: left;
 }
 </style>
