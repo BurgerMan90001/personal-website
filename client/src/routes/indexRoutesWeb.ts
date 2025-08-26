fb08735097828afc3d33337ef6958b97948d5293
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-08-26 14:00:45 -0500

    moved env.d.ts to client

--- client/src/routes/indexRoutesWeb.ts
+++ client/src/routes/indexRoutesWeb.ts
@@ -3,6 +3,7 @@ import * as indexControllerWeb from '../controllers/indexControllerWeb';
 
 
 const router:WebRouter = createRouter({
+  
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     indexControllerWeb.home,
