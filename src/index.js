3ecc016ecd7fe4e536bface8b1537d48123024f0
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2026-01-27 16:14:00 -0600

    api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dino from "./assets/images/website-dinosaur.png";
 import "./assets/css/main.css";
-const img = document.createElement("img");
-img.src = dino;
 
-document.body.appendChild(img);
+const img = document.querySelector(".img");
+img.src = "https://picsum.photos/200";
+fetch("https://picsum.photos/200").then(function (response) {
+  //img.src = response.url;
+});
