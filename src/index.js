c9322135f02a635ab84f9d64a18f7b029a4f03c5
BurgerMan90001 <paulcasigay@gmail.com>
2026-01-27 19:14:54 -0600

    add url

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import "./assets/css/main.css";
 const img = document.querySelector(".img");
 img.src = "https://picsum.photos/200";
 fetch("https://picsum.photos/200").then(function (response) {
-  //img.src = response.url;
+  img.src = response.url;
 });
 
