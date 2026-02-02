76bbdd0a0c21b7dfd7de8ebf18adddc1512ccc5b
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2026-02-02 08:27:41 -0600

    fix weird git thing

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
-import dino from "./assets/images/website-dinosaur.png";
+//import dino from "./assets/images/website-dinosaur.png";
 import "./assets/css/main.css";
-
-<<<<<<< HEAD
-const img = document.querySelector(".img");
-img.src = "https://picsum.photos/200";
-fetch("https://picsum.photos/200").then(function (response) {
-  //img.src = response.url;
-});
-=======
-document.body.appendChild(img);
-
->>>>>>> 9ae8c608bdf196a9b042e676f52b09b2c0c2240e
-=======
-
 
 const img = document.querySelector(".img");
 img.src = "https://picsum.photos/200";
 fetch("https://picsum.photos/200").then(function (response) {
   img.src = response.url;
 });
-
->>>>>>> 1bb040fd749259a2fe61f1bc0fa8d6e32362a28a
