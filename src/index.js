d42a3a96d424ab31b8e0b2f254c6b762416c030b
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2026-02-03 09:05:53 -0600

    Revert "fix weird git thing"
    This reverts commit 76bbdd0a0c21b7dfd7de8ebf18adddc1512ccc5b.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import _ from "lodash";
 import "./assets/main.css";
 /*
@@ -16,3 +17,29 @@ fetch("https://picsum.photos/200")
   })
   .catch((error) => console.error(error));
 */
+=======
+<<<<<<< HEAD
+import dino from "./assets/images/website-dinosaur.png";
+import "./assets/css/main.css";
+
+<<<<<<< HEAD
+const img = document.querySelector(".img");
+img.src = "https://picsum.photos/200";
+fetch("https://picsum.photos/200").then(function (response) {
+  //img.src = response.url;
+});
+=======
+document.body.appendChild(img);
+
+>>>>>>> 9ae8c608bdf196a9b042e676f52b09b2c0c2240e
+=======
+
+
+const img = document.querySelector(".img");
+img.src = "https://picsum.photos/200";
+fetch("https://picsum.photos/200").then(function (response) {
+  img.src = response.url;
+});
+
+>>>>>>> 1bb040fd749259a2fe61f1bc0fa8d6e32362a28a
+>>>>>>> parent of 76bbdd0 (fix weird git thing)
